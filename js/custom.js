4f268fd5eff7d2da58f33f170385f3c5dc9c7673
mamunur-roshid <nijummamun7@gmail.com>
2023-09-19 00:16:05 +0600

    images path change

--- js/custom.js
+++ js/custom.js
@@ -4,9 +4,9 @@ const eye_icon = document.getElementById("eye_icon");
 eye_icon.addEventListener("click", () => {
   if (password.type == "password") {
     password.type = "text";
-    eye_icon.src = "/images/eye-open.png";
+    eye_icon.src = "images/eye-open.png";
   } else {
     password.type = "password";
-    eye_icon.src = "/images/eye-close.png";
+    eye_icon.src = "images/eye-close.png";
   }
 });
